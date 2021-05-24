ab89fbae63fa3548087779a3e4aeb7a1560d4e55
ojeelafriend <ts8irolon@gmail.com>
2021-05-24 09:12:44 -0300

    create dir config and insert passport local in passport.js

---
+++ config/passport.js
@@ -0,0 +1,29 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const user = {
+	id: 10,
+	username: 'ojeeldev',
+	password: 'ojeeldev',
+};
+
+passport.use(
+	new LocalStrategy(
+		{
+			userfield: 'username',
+		},
+		(username, password, done) => {
+			if (username === user.username && password === user.password) {
+				done(null, user);
+			}
+			done(null, false);
+		}
+	)
+);
+
+passport.serializeUser((user, done) => {
+	done(null, user.id);
+});
+passport.deserializeUser((id, done) => {
+	done(null, user);
+});
